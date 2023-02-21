5c5b49622391b7eba843ea6ccd7946875f4758fe
Ishan4u <mohamedishan44@gmail.com>
2023-02-21 12:42:41 +0530

    Define express application

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const fileupload = require("express-fileupload");
 const pdfParse = require("pdf-parse");
+
+const app = express();
+
+app.use("/", express.static("public"));
+
+app.listen(3000);
